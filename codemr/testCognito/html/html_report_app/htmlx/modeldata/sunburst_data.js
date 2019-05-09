bd48399d88fde6b4afa6bd9bda9edcfbb11033fa
Andrea <37840923+Andreapava@users.noreply.github.com>
2019-05-09 19:17:53 +0200

    codeMR per matteo (la path nel commento)
    AndroidStudioProjects/testCognito/codemr/testCognito/html/main_report/htmlx/lbd/dashboard.html

---
+++ codemr/testCognito/html/html_report_app/htmlx/modeldata/sunburst_data.js
@@ -0,0 +1,147 @@
+function EQ_GET_DATA(){ 
+	  var ret = {
+"name": " testCognito working set: {app}", "value": "896", 
+"prmetrics":{"5":1,"6":1,"7":1,"8":1,"9":3,"10":1,"11":1}
+,
+"prmetricvalues":{"5":0,"6":16,"7":3,"8":896,"9":30,"10":1,"11":79}
+,
+"children": [ {
+"name": "com.example.testcognito", "value": "896", 
+"pmetrics":{"4":3,"12":2,"13":1,"14":3,"3":1,"1":1,"0":2,"6":3,"8":2,"2":2,"15":1,"16":1}
+,
+"pmetricvalues":{"4":3,"12":10,"13":0,"14":16,"3":1,"1":1,"0":2,"17":0.0,"6":16,"18":0.0,"8":896,"19":1.0,"2":2,"15":0,"16":141}
+,
+"children": [ {
+"name": "Connector","key": "t","value": "100", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":3,"27":1,"28":1,"29":4,"30":4,"31":1,"32":3,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":3,"1":1}
+,
+"metricvalues":{"20":0,"21":0,"38":4,"22":11,"23":0.0,"24":45,"25":0,"26":1,"0":3,"27":15,"28":18,"29":0.868,"30":0.89,"31":0,"32":0.781,"39":0,"33":0,"34":3,"40":4,"35":0,"36":0,"16":17,"37":0,"4":2,"8":100,"2":1,"3":3,"1":1}
+}
+,{
+"name": "WorkflowAdapter","key": "v","value": "63", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":2,"0":2,"27":1,"28":1,"29":1,"30":3,"31":1,"32":2,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":2,"8":2,"2":1,"3":2,"1":2}
+,
+"metricvalues":{"20":5,"21":0,"38":1,"22":3,"23":0.0,"24":18,"25":0,"26":2,"0":2,"27":6,"28":13,"29":0.4,"30":0.733,"31":0,"32":0.639,"39":0,"33":0,"34":7,"40":1,"35":0,"36":5,"16":6,"37":0,"4":2,"8":63,"2":1,"3":2,"1":2}
+}
+,{
+"name": "MainActivity","key": "p","value": "85", 
+"metrics":{"20":4,"21":1,"22":1,"23":1,"24":2,"25":1,"26":3,"0":4,"27":1,"28":1,"29":5,"30":5,"31":1,"32":2,"33":1,"34":1,"35":1,"36":4,"16":1,"37":1,"4":2,"8":2,"2":4,"3":2,"1":3}
+,
+"metricvalues":{"20":27,"21":0,"38":2,"22":5,"23":0.0,"24":56,"25":0,"26":10,"0":4,"27":5,"28":47,"29":1.0,"30":1.5,"31":1,"32":0.625,"39":2,"33":2,"34":38,"40":0,"35":1,"36":25,"16":9,"37":0,"4":2,"8":85,"2":4,"3":2,"1":3}
+}
+,{
+"name": "ConnectorActivity","key": "z","value": "235", 
+"metrics":{"20":5,"21":1,"22":1,"23":1,"24":2,"25":1,"26":3,"0":5,"27":1,"28":2,"29":4,"30":4,"31":1,"32":3,"33":2,"34":2,"35":1,"36":4,"16":2,"37":1,"4":2,"8":2,"2":5,"3":3,"1":3}
+,
+"metricvalues":{"20":35,"21":1,"38":6,"22":9,"23":0.0,"24":201,"25":0,"26":10,"0":5,"27":14,"28":95,"29":0.833,"30":0.898,"31":3,"32":0.786,"39":4,"33":6,"34":87,"40":2,"35":0,"36":29,"16":33,"37":0,"4":2,"8":235,"2":5,"3":3,"1":3}
+}
+,{
+"name": "AvailableConnectorRecycleViewAdapter","key": "n","value": "40", 
+"metrics":{"20":2,"21":1,"22":1,"23":1,"24":1,"25":1,"26":2,"0":2,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":2,"16":1,"37":1,"4":1,"8":1,"2":2,"3":1,"1":2}
+,
+"metricvalues":{"20":9,"21":0,"38":3,"22":2,"23":0.0,"24":22,"25":0,"26":2,"0":2,"27":4,"28":14,"29":0.5,"30":0.5,"31":0,"32":0.583,"39":2,"33":2,"34":10,"40":1,"35":0,"36":7,"16":4,"37":0,"4":1,"8":40,"2":2,"3":1,"1":2}
+}
+,{
+"name": "AuthenticationActivity","key": "k","value": "57", 
+"metrics":{"20":2,"21":1,"22":1,"23":1,"24":2,"25":1,"26":3,"0":3,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":2,"16":1,"37":1,"4":2,"8":2,"2":2,"3":1,"1":3}
+,
+"metricvalues":{"20":8,"21":0,"22":1,"23":0.0,"24":55,"25":0,"26":10,"0":3,"27":2,"28":16,"29":0.0,"30":0.0,"31":0,"32":0.25,"33":0,"34":14,"35":0,"36":8,"16":3,"37":0,"4":2,"8":57,"2":2,"3":1,"1":3}
+}
+,{
+"name": "ClientFactory","key": "s","value": "19", 
+"metrics":{"20":1,"21":1,"22":1,"23":1,"24":1,"25":1,"26":1,"0":1,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":1,"16":1,"37":1,"4":1,"8":1,"2":1,"3":1,"1":1}
+,
+"metricvalues":{"20":4,"21":0,"38":3,"22":0,"23":0.0,"24":17,"25":0,"26":1,"0":1,"27":0,"28":7,"29":0.0,"30":0.0,"31":1,"32":0.25,"39":0,"33":0,"34":5,"40":3,"35":2,"36":4,"16":3,"37":0,"4":1,"8":19,"2":1,"3":1,"1":1}
+}
+,{
+"name": "AddWorkflowActivity","key": "x","value": "58", 
+"metrics":{"20":3,"21":1,"22":1,"23":1,"24":1,"25":1,"26":3,"0":3,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":3,"16":1,"37":1,"4":2,"8":2,"2":3,"3":1,"1":3}
+,
+"metricvalues":{"20":19,"21":0,"38":1,"22":1,"23":0.0,"24":37,"25":0,"26":10,"0":3,"27":3,"28":34,"29":0.0,"30":0.0,"31":1,"32":0.444,"39":1,"33":2,"34":32,"40":0,"35":0,"36":17,"16":7,"37":0,"4":2,"8":58,"2":3,"3":1,"1":3}
+}
+,{
+"name": "ConnParametersRecycleViewAdapter","key": "l","value": "69", 
+"metrics":{"20":3,"21":1,"22":1,"23":1,"24":1,"25":1,"26":2,"0":3,"27":1,"28":1,"29":3,"30":3,"31":1,"32":2,"33":1,"34":1,"35":1,"36":2,"16":1,"37":1,"4":2,"8":2,"2":3,"3":2,"1":2}
+,
+"metricvalues":{"20":11,"21":0,"38":2,"22":5,"23":0.0,"24":36,"25":0,"26":2,"0":3,"27":7,"28":18,"29":0.762,"30":0.75,"31":1,"32":0.696,"39":1,"33":1,"34":12,"40":1,"35":0,"36":10,"16":12,"37":0,"4":2,"8":69,"2":3,"3":2,"1":2}
+}
+,{
+"name": "SetConnectorActivity","key": "y","value": "127", 
+"metrics":{"20":5,"21":1,"22":1,"23":5,"24":2,"25":1,"26":3,"0":5,"27":1,"28":2,"29":3,"30":4,"31":1,"32":3,"33":1,"34":1,"35":1,"36":4,"16":2,"37":1,"4":2,"8":2,"2":5,"3":3,"1":3}
+,
+"metricvalues":{"20":31,"21":1,"38":3,"22":14,"23":0.833,"24":110,"25":0,"26":10,"0":5,"27":12,"28":52,"29":0.8,"30":0.991,"31":2,"32":0.781,"39":2,"33":3,"34":47,"40":1,"35":0,"36":28,"16":23,"37":1,"4":2,"8":127,"2":5,"3":3,"1":3}
+}
+,{
+"name": "ActiveConnectorRecycleViewAdapter","key": "q","value": "43", 
+"metrics":{"20":2,"21":1,"22":1,"23":1,"24":1,"25":1,"26":2,"0":2,"27":1,"28":1,"29":1,"30":1,"31":1,"32":1,"33":1,"34":1,"35":1,"36":2,"16":1,"37":1,"4":1,"8":1,"2":2,"3":1,"1":2}
+,
+"metricvalues":{"20":9,"21":0,"38":3,"22":2,"23":0.0,"24":25,"25":0,"26":2,"0":2,"27":5,"28":16,"29":0.4,"30":0.5,"31":0,"32":0.6,"39":2,"33":2,"34":11,"40":1,"35":0,"36":7,"16":5,"37":0,"4":1,"8":43,"2":2,"3":1,"1":2}
+}
+]
+ }
+]
+ }
+;
+return ret;
+}
+var EQ_METRIC_MAP = {};
+EQ_METRIC_MAP["C3"] =0;
+EQ_METRIC_MAP["Complexity"] =1;
+EQ_METRIC_MAP["Coupling"] =2;
+EQ_METRIC_MAP["Lack of Cohesion"] =3;
+EQ_METRIC_MAP["Size"] =4;
+EQ_METRIC_MAP["Number of Highly Problematic Classes"] =5;
+EQ_METRIC_MAP["Number of Entities"] =6;
+EQ_METRIC_MAP["Number of Problematic Classes"] =7;
+EQ_METRIC_MAP["Class Lines of Code"] =8;
+EQ_METRIC_MAP["Number of External Packages"] =9;
+EQ_METRIC_MAP["Number of Packages"] =10;
+EQ_METRIC_MAP["Number of External Entities"] =11;
+EQ_METRIC_MAP["Efferent Coupling"] =12;
+EQ_METRIC_MAP["Number of Interfaces"] =13;
+EQ_METRIC_MAP["Number of Classes"] =14;
+EQ_METRIC_MAP["Afferent Coupling"] =15;
+EQ_METRIC_MAP["Weighted Method Count"] =16;
+EQ_METRIC_MAP["Normalized Distance"] =17;
+EQ_METRIC_MAP["Abstractness"] =18;
+EQ_METRIC_MAP["Instability"] =19;
+EQ_METRIC_MAP["Coupling Between Object Classes"] =20;
+EQ_METRIC_MAP["Access to Foreign Data"] =21;
+EQ_METRIC_MAP["Number of Fields"] =22;
+EQ_METRIC_MAP["Specialization Index"] =23;
+EQ_METRIC_MAP["Class-Methods Lines of Code"] =24;
+EQ_METRIC_MAP["Number of Children"] =25;
+EQ_METRIC_MAP["Depth of Inheritance Tree"] =26;
+EQ_METRIC_MAP["Number of Methods"] =27;
+EQ_METRIC_MAP["Response For a Class"] =28;
+EQ_METRIC_MAP["Lack of Tight Class Cohesion"] =29;
+EQ_METRIC_MAP["Lack of Cohesion of Methods"] =30;
+EQ_METRIC_MAP["Number of Static Fields"] =31;
+EQ_METRIC_MAP["Lack of Cohesion Among Methods(1-CAM)"] =32;
+EQ_METRIC_MAP["CBO App"] =33;
+EQ_METRIC_MAP["Simple Response For a Class"] =34;
+EQ_METRIC_MAP["Number of Static Methods"] =35;
+EQ_METRIC_MAP["CBO Lib"] =36;
+EQ_METRIC_MAP["Number of Overridden Methods"] =37;
+EQ_METRIC_MAP["Degree"] =38;
+EQ_METRIC_MAP["OutDegree"] =39;
+EQ_METRIC_MAP["InDegree"] =40;
+var EQ_SELECTED_CLASS_METRIC 		= "C3";
+var EQ_SELECTED_PACKAGE_METRIC 	= "C3";
+var EQ_SELECTED_PROJECT_METRIC 	= "Class Lines of Code";
+var EQ_CLASS_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_CLASS_METRIC];
+var EQ_PACKAGE_METRIC_INDEX	= EQ_METRIC_MAP[EQ_SELECTED_PACKAGE_METRIC];
+var EQ_PROJECT_METRIC_INDEX 	= EQ_METRIC_MAP[EQ_SELECTED_PROJECT_METRIC];
+var EQ_COLOR_OF_LEVELS = ["#1F77B4","#007F24","#62BF18","#FFC800","#FF5B13","#E50000"];
+var EQ_CLASS_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Class Lines of Code","Weighted Method Count","Coupling Between Object Classes","Access to Foreign Data","Number of Fields","Specialization Index","Class-Methods Lines of Code","Number of Children","Depth of Inheritance Tree","Number of Methods","Response For a Class","Lack of Tight Class Cohesion","Lack of Cohesion of Methods","Number of Static Fields","Lack of Cohesion Among Methods(1-CAM)","CBO App","Simple Response For a Class","Number of Static Methods","CBO Lib","Number of Overridden Methods","Degree","OutDegree","InDegree"];
+var EQ_PACKAGE_METRICS = ["C3","Complexity","Coupling","Lack of Cohesion","Size","Number of Entities","Class Lines of Code","Efferent Coupling","Number of Interfaces","Number of Classes","Afferent Coupling","Weighted Method Count","Normalized Distance","Abstractness","Instability"];
+var EQ_PROJECT_METRICS = ["Number of Highly Problematic Classes","Number of Entities","Number of Problematic Classes","Class Lines of Code","Number of External Packages","Number of Packages","Number of External Entities"];
+function EQ_GET_COLOR(d) {
+if(d.metrics)
+return EQ_COLOR_OF_LEVELS[d.metrics[EQ_CLASS_METRIC_INDEX]];
+if(d.pmetrics)
+return EQ_COLOR_OF_LEVELS[d.pmetrics[EQ_PACKAGE_METRIC_INDEX]];
+if(d.prmetrics)
+return EQ_COLOR_OF_LEVELS[d.prmetrics[EQ_PROJECT_METRIC_INDEX]];
+return EQ_COLOR_OF_LEVELS[0];
+}
---
+++ codemr/testCognito/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"testCognito", "Number of Highly Problematic Classes":0, "Number of Entities":16, "Number of Problematic Classes":3, "Class Lines of Code":896, "Number of External Packages":47, "Number of Packages":1, "Number of External Entities":206}
